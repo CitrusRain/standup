00f83749a382f92918200123a3da55fcbb8c7991
Citrus <ritolink@gmail.com>
2025-09-19 22:00:45 -0400

    Game Structure Progress
    Set up menu screen layout for between levels
    -Select venue: has button to go onstage
    -Manage Routine: has split container to hold what we have, and what we're bringing with us, as well as a joke details panel
    -Shop: Currently empty
    
    Added a global scene we will be able to store data in
    
    Added a catalog that we will store all jokes in in sets
    
    Cards have a use counter to count how many times the joke is told

--- Handlers/joke_handler.gd
+++ Handlers/joke_handler.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var joke_file = ClydeDialogue.new()
+@onready var joke_node = Card
 @export var dialogue_holder : PackedScene ##The object that is to be spawned
 @onready var stage_dialogue_box: Control = get_tree().get_first_node_in_group("StageSpeechBox")
 @onready var ui: Control = get_tree().get_first_node_in_group("UI")
@@ -21,7 +22,13 @@ func show_next_line() -> void:
 	print(idk)
 	ui.next_visible(idk.get("type") != "end")
 	if idk.get("type") != "end":
-		speechbox.set_text(str(idk.get("speaker"), ": " , idk.get("text")))
+		print(idk.get("speaker"))
+		if idk.get("speaker") == null:
+			speechbox.set_text(idk.get("text"))
+		else:
+			speechbox.set_text(str(idk.get("speaker"), ": " , idk.get("text")))
+	else:
+		joke_node.uses += 1 ##Finished telling the joke one more time than before
 	for lines in stage_dialogue_box.get_children():
 		lines.queue_free()
 	stage_dialogue_box.add_child(speechbox)
--- cards/card.gd
+++ cards/card.gd
@@ -1,14 +1,16 @@
-extends Node2D
-
+extends Control
+class_name Card
 
 @onready var batman: Node2D = get_tree().get_first_node_in_group("JokeHandler")
 @onready var dialogue = ClydeDialogue.new()
-
+@onready var uses := 0 ## The number of times this joke has been told
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_paper_pressed() -> void:
-	batman.joke_file.load_dialogue(str("res://DialogueSystem/Jokes/" , str(get_meta("ClydeFile"),".clyde")))
-	batman.show_next_line()
+	if get_parent().get_name() == "Hand":
+		batman.joke_file.load_dialogue(str("res://DialogueSystem/Jokes/" , str(get_meta("ClydeFile"),".clyde")))
+		batman.joke_node = self
+		batman.show_next_line()
